06ac6d175e9757ac7ecb1f03337eaf10382efe0a
AquilesDias <kilesax@gmail.com>
2023-10-11 09:20:22 -0300

    feat: criando exception para email service

---
+++ src/main/java/com/aquiles/serviceemail/core/exceptions/EmailServiceException.java
@@ -0,0 +1,12 @@
+package com.aquiles.serviceemail.core.exceptions;
+
+public class EmailServiceException extends RuntimeException{
+
+    public EmailServiceException(String message){
+        super(message);
+    }
+
+    public EmailServiceException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
